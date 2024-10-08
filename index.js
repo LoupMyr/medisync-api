1b90e599af43d376a4b9ef8ef94b6b44880b83c1
Jsobkowiak <jonathan.sobkowiak62@gmail.com>
2024-10-08 16:54:24 +0200

    prisma

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000;
-let db = require("./databases.js")
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
+const { PrismaClient } = require('@prisma/client');
 app.use(express.json());
 const config = {
   headers: {
@@ -11,46 +11,87 @@ const config = {
   }
 };
 
+const prisma = new PrismaClient()
 
-
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.send('Hello World!');
+
+  const allUsers = await prisma.user.findMany();
+
+  console.log(allUsers);
 })
 
 
 app.post('/auth', async (req, res) => {
   const { email, password } = req.body;
 
-
   if (!email || !password) {
     return res.status(400).json({ "error": "Missing required fields: email, password" });
   }
 
 
-  console.log(hashPasswordSync(password));
-  const sql = "SELECT * FROM users WHERE email = ?";
 
-  db.execute(sql, [email], async (err, results) => {
-    if (err) {
-      return res.status(400).json({ "error": err.message });
+  const match = await bcrypt.compare(password, row.password);
+  if (!match) {
+    return res.status(401).json({ "error": "Invalid email or password" });
+  }
+
+
+  const token = jwt.sign({ id: row.id }, "test", {
+    algorithm: 'HS256',
+    expiresIn: '1h',
+  });
+  res.status(201).json({
+    "message": "success",
+    "token": token,
+    "data": { id: row.id, email: row.email },
+  });
+
+
+});
+
+
+
+app.post("/postUser", async (req, res) => {
+  let { email, password, pseudo } = req.body;
+
+  // Vérification de la présence des champs requis
+  if (!email || !password || !pseudo) {
+    return res.status(400).json({ "error": "Missing required fields: email, password, pseudo" });
+  }
+
+  // Validation du format de l'email avec une regex
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ "error": "Invalid email format" });
+  }
+
+  // Vérification de la longueur du mot de passe
+  if (password.length < 8) {
+    return res.status(400).json({ "error": "Invalid password format, require 8 characters" });
+  }
+  // Hachage du mot de passe
+  password = hashPasswordSync(password);
+
+  // Vérification si l'email existe déjà dans la base de données
+  const emailCheck = "SELECT * FROM users WHERE email = ?";
+
+  db.execute(emailCheck, [email], async (err, results) => {
+    if (results) {
+      return res.status(500).json({ "error": "Database error" });
     }
-    if (results.length === 0) {
-      return res.status(404).json({ "error": "User not found" });
-    }
-    const row = results[0];
-    const match = await bcrypt.compare(password, row.password);
-    if (!match) {
-      return res.status(401).json({ "error": "Invalid email or password" });
-    }
-    const token = jwt.sign({ id: row.id }, "test", {
-      algorithm: 'HS256',
-      expiresIn: '1h',
+
+
+
+    /*const sql = "INSERT INTO users(email, password) VALUES (?, ?)";
+    const params = [email, password];
+    db.execute(sql, params, function (err, results) {
+      if (err) {
+        return res.status(500).json({ "error": "Failed to insert user" });
+      }
+
     });
-    res.json({
-      "message": "success",
-      "token": token,
-      "data": { id: row.id, email: row.email },
-    });
+    */
   });
 });
 
